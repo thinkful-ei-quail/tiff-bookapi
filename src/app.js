2622b551e2a8c0a110fa2a1483c370a619806738
Tiff Joy <tiffanysummerford@gmail.com>
2020-08-27 23:15:56 -0400

    'works'

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const errorHandler = require('./error-handler');
 const bookmarkRouter = require('./bookmarks/bookmarkRouter');
-const app = express();
 const morganOption = (process.env.NODE_ENV === 'production') ? 'tiny' : 'common';
+
+
+const app = express();
 
 app.use(morgan(morganOption));
 app.use(cors());
@@ -13,5 +16,7 @@ app.use(helmet());
 app.use(express.json());
   
 app.use(bookmarkRouter);
+
+app.use(errorHandler);
   
 module.exports = app;
--- src/bookmarks/bookmarkRouter.js
+++ src/bookmarks/bookmarkRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { v4: uuid } = require('uuid');
 const { isWebUri } = require('valid-url');
 const logger = require('../logger');
-const bookmarks = require('../STORE');
+const STORE = require('../STORE');
 
 const bookmarkRouter = express.Router();
 const bodyParser = express.json();
@@ -13,8 +13,8 @@ bookmarkRouter.route('/').get((req, res) => {
 
 bookmarkRouter.route('/bookmarks')
   .get((req, res) => {
-    res.status(200).json(bookmarks);
-})
+    res.status(200).json(STORE.bookmarks);
+  })
   .post(bodyParser, (req, res) => {
     const { title, url, description = '', rating } = req.body;
 
@@ -27,12 +27,12 @@ bookmarkRouter.route('/bookmarks')
       return res.json(400).send('Invalid data');
     }
     if (!isWebUri(url)) {
-      logger.error(`Invalid url '${url}' supplied`)
-      return res.status(400).send(`'url' must be a valid URL`)
+      logger.error(`Invalid url '${url}' supplied`);
+      return res.status(400).send(`'url' must be a valid URL`);
     }
     if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
-      logger.error(`Invalid rating '${rating}' supplied`)
-      return res.status(400).send(`'Rrating' must be a number between 0 and 5`)
+      logger.error(`Invalid rating '${rating}' supplied`);
+      return res.status(400).send(`'Rrating' must be a number between 0 and 5`);
     }
 
     const id = uuid();
@@ -44,7 +44,7 @@ bookmarkRouter.route('/bookmarks')
       rating
     };
 
-    bookmarks.push(bookmark)
+    STORE.bookmarks.push(bookmark)
     logger.info(`Bookmark with ${id} created!`)
     return res.status(201).location(`http://localhost:8000/list/${id}`).json({ id });
   });
@@ -52,7 +52,7 @@ bookmarkRouter.route('/bookmarks')
 bookmarkRouter.route('/bookmarks/:id')
   .get((req, res) => {
     const { id } = req.params;
-    const bookmark = bookmarks.find(book => book.id === id);
+    const bookmark = STORE.bookmarks.find(book => book.id === id);
 
     if (!bookmark) {
       logger.error(`Bookmark with ${id} not found`);
@@ -63,13 +63,13 @@ bookmarkRouter.route('/bookmarks/:id')
 
   .delete((req, res) => {
     const { id } = req.params
-    const index = bookmarks.findIndex(book => book.id === id);
+    const index = STORE.bookmarks.findIndex(book => book.id === id);
 
     if (!index === -1) {
       logger.error(`Bookmark with id ${id} not found`);
       return res.status(404).send('Not found');
     }
-    bookmarks.splice(index, 1);
+    STORE.bookmarks.splice(index, 1);
     logger.info(`Bookmark with id ${id} deleted`);
     res.status(204).end();
   });
